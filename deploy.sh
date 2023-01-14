1f811126bf3acc06666bf6850267f5e7d569cd9c
unknown <kariimgamal1997@gmail.com>
2023-01-14 19:12:39 +0200

    Updating CV

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:KarimGamal97/Kariim_Portfolio.git main:gh-pages
+git push -f git@github.com:KarimGamal97/Kariim_Portfolio.git master:gh-pages
 
 cd -
